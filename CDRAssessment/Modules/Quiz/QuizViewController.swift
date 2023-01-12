5921c514460bda2e07f9e963532b3547faf28827
DiegoHSO <diegohenrique.so2000@gmail.com>
2023-01-12 14:09:53 -0300

    more modifications to quiz logic

--- CDRAssessment/Modules/Quiz/QuizViewController.swift
+++ CDRAssessment/Modules/Quiz/QuizViewController.swift
@@ -16,16 +16,6 @@ class QuizViewController: BaseViewController, Storyboarded {
     
     @IBOutlet weak var tableView: UITableView!
     
-    init(coordinator: QuizCoordinator, viewModel: QuizViewModel) {
-        self.coordinator = coordinator
-        self.viewModel = viewModel
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
