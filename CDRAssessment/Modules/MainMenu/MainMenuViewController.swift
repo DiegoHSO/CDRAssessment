67dc07898668dafb5f40d7985e4a62ed8ddca21d
DiegoHSO <diegohenrique.so2000@gmail.com>
2023-01-17 17:33:46 -0300

    finished Main Menu redesign

--- CDRAssessment/Modules/MainMenu/MainMenuViewController.swift
+++ CDRAssessment/Modules/MainMenu/MainMenuViewController.swift
@@ -9,68 +9,65 @@ import UIKit
 
 class MainMenuViewController: UIViewController, Storyboarded {
     
+    @IBOutlet weak var leftUpperBubbleView: UIView!
+    @IBOutlet weak var rightUpperBubbleView: UIView!
+    @IBOutlet weak var leftLowerBubbleView: UIView!
+    @IBOutlet weak var rightLowerBubbleView: UIView!
+    @IBOutlet weak var rightCenterBubbleView: UIView!
+    @IBOutlet weak var leftCenterBubbleView: UIView!
+    @IBOutlet weak var tableView: UITableView!
     weak var coordinator: MainMenuCoordinator?
-
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var subtitleLabel: UILabel!
-//    @IBOutlet weak var developerLabel: UILabel!
-    @IBOutlet weak var startButton: UIButton!
-    @IBOutlet weak var removeAdsView: UIView!
-    @IBOutlet weak var removeAdsButton: UIButton!
-    @IBOutlet weak var referenceButton: UIButton!
-
-    @IBOutlet weak var externalView: UIView!
-    @IBOutlet weak var internalView: UIView!
-    
-    @IBAction func startAction(_ sender: UIButton) {
-        coordinator?.goToQuiz()
-    }
-    
-    @IBAction func referenceAction(_ sender: UIButton) {
-        coordinator?.goToReferencePage()
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureViews()
-        titleLabel.text = NSLocalizedString("title", comment: "")
-        subtitleLabel.text = NSLocalizedString("subtitle", comment: "")
-        referenceButton.setTitle(NSLocalizedString("reference", comment: ""), for: .normal)
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        setupBubbleViews()
     }
     
-    private func configureViews() {
-        let border = CAShapeLayer()
-        border.strokeColor = UIColor.orange.cgColor
-        border.lineDashPattern = [6, 3]
-        border.frame = internalView.bounds
-        border.fillColor = nil
-        border.path = UIBezierPath(ovalIn: internalView.bounds).cgPath
-        internalView.layer.addSublayer(border)
-        
-//        externalView.layer.masksToBounds = false
-//        internalView.layer.masksToBounds = false
-        externalView.layer.cornerRadius = externalView.frame.height / 2
-        internalView.layer.cornerRadius = internalView.frame.height / 2
-        
-        externalView.layer.shadowColor = UIColor.darkGray.cgColor
-        externalView.layer.shadowOffset = CGSize(width: 0, height: 5.0)
-        externalView.layer.shadowOpacity = 0.1
-        externalView.layer.shadowRadius = 5.0
-        
-        startButton.setTitle(NSLocalizedString("start", comment: ""), for: .normal)
-        startButton.titleLabel?.layer.shadowOffset = CGSize(width: 0.2, height: 0.5)
-        startButton.titleLabel?.layer.shadowOpacity = 0.7
-        startButton.titleLabel?.layer.shadowRadius = 0
-        removeAdsView.layer.cornerRadius = removeAdsView.frame.height / 2
-//        removeAdsButton.setTitle("Remover\nAnúncios", for: .normal)
-//        externalView.clipsToBounds = true
-//        internalView.clipsToBounds = true
-        
+    private func setupBubbleViews() {
+        leftUpperBubbleView.layer.cornerRadius = leftUpperBubbleView.frame.height / 2
+        rightUpperBubbleView.layer.cornerRadius = rightUpperBubbleView.frame.height / 2
+        leftLowerBubbleView.layer.cornerRadius = leftLowerBubbleView.frame.height / 2
+        rightLowerBubbleView.layer.cornerRadius = rightLowerBubbleView.frame.height / 2
+        leftCenterBubbleView.layer.cornerRadius = leftCenterBubbleView.frame.height / 2
+        rightCenterBubbleView.layer.cornerRadius = rightCenterBubbleView.frame.height / 2
     }
+    
     override func viewWillAppear(_ animated: Bool) {
         coordinator?.removeAllChildCoordinators()
     }
     
 }
 
+extension MainMenuViewController: MainMenuDelegate {
+    func didTapReferenceButton() {
+        coordinator?.goToReferencePage()
+    }
+    
+    func didTapStartButton() {
+        coordinator?.goToQuiz()
+    }
+    
+    func didTapRemoveAdsButton() {
+        
+    }
+}
+
+extension MainMenuViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "mainCell") as? MainMenuTableViewCell else { return UITableViewCell() }
+        
+        cell.delegate = self
+        return cell
+    }
+}
+
+extension MainMenuViewController: UITableViewDelegate {
+    
+}
